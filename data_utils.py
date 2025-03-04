8f6b586be3b348bb9c80c2d078b9426b4085fb14
Ashutosh Joshi <ashutoshjoshi179@gmail.com>
2025-03-04 12:25:20 +0530

    Initial structure for data_utils.py.

---
+++ data_utils.py
@@ -0,0 +1,20 @@
+class DataReduction:
+
+    def __init__():
+        """
+        Initial Constructor.
+        """
+        
+        
+    def average_frames():
+        """
+        Average frames for each event number.
+        """
+        return
+    
+    
+    def rename_frames():
+        """
+        Renames frames according to frame type.
+        """
+        return
